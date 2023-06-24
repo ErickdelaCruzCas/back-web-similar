0800e4644105a88023bd2a5c9c9028a54ab1a1d9
e_ecruz <e_ecruz@mercadona.com>
2023-06-24 15:23:57 +0200

    init repository

---
+++ src/test/java/com/prueba/tecnica/similar/SimilarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.tecnica.similar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimilarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

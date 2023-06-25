3f3b6960e14dbaa806d3d15fd5f110d4ac1384df
e_ecruz <e_ecruz@mercadona.com>
2023-06-25 11:45:16 +0200

    init repo

---
+++ src/main/java/com/prueba/backwebsimilar/BackWebSimilarApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.backwebsimilar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackWebSimilarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackWebSimilarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba/backwebsimilar/BackWebSimilarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.backwebsimilar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackWebSimilarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
